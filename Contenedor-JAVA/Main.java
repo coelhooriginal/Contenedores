939f4a32c310874f8b265e82fef79bdad0bd7947
Rodrigo Coelho <coelhooriginal@gmail.com>
2025-10-05 21:22:53 +0200

    Agregar todas las carpetas de contenedores

---
+++ Contenedor-JAVA/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Creamos el primer contenedor java de Rodrigo Coelho Vila");
+    }
+}
+
---
+++ Contenedor-PYTHON/app.py
@@ -0,0 +1,1 @@
+print("Creamos el primer contenedor python de Rodrigo Coelho Vila")
